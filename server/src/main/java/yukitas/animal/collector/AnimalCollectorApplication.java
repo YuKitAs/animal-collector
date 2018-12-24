3679606239372d3bdaa67a80226d7eef57de790f
YuKitAs <moncher.merci@gmail.com>
2018-12-24 22:55:55 +0100

    Init server project

---
+++ server/src/main/java/yukitas/animal/collector/AnimalCollectorApplication.java
@@ -0,0 +1,14 @@
+package yukitas.animal.collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalCollectorApplication.class, args);
+	}
+
+}
+
