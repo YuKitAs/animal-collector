67fd4b374148f0eb115dc31c82bc3b6fb166ee36
YuKitAs <moncher.merci@gmail.com>
2019-03-03 19:44:49 +0100

    Modify data type for test

--- client/app/src/main/java/yukitas/animal/collector/model/Photo.kt
+++ client/app/src/main/java/yukitas/animal/collector/model/Photo.kt
@@ -2,22 +2,7 @@ package yukitas.animal.collector.model
 
 data class Photo(
         val id: String,
-        val content: ByteArray,
+        val content: String,
         val description: String,
         val createdAt: String,
-        val location: Location) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Photo
-
-        if (!content.contentEquals(other.content)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return content.contentHashCode()
-    }
-}
+        val location: Location)
