c2876eb9fe84ff6c7b9b93e0bd4be702fda8e66d
YuKitAs <moncher.merci@gmail.com>
2019-03-31 21:51:20 +0200

    Display tags

--- client/app/src/main/java/yukitas/animal/collector/view/adapter/AnimalsAdapter.kt
+++ client/app/src/main/java/yukitas/animal/collector/view/adapter/AnimalsAdapter.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.template_animal_tag.view.*
 import yukitas.animal.collector.R
 import yukitas.animal.collector.model.Animal
 
@@ -22,7 +23,20 @@ class AnimalsAdapter(private val context: Context) : BaseAdapter() {
     @SuppressLint("ViewHolder")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.item_animal, parent, false)
-        binding.animal = animals[position]
+
+        val animal = animals[position]
+        binding.animal = animal
+
+        animal.tags.forEach {
+            if (it.isNotEmpty()) {
+                val tagView = LayoutInflater.from(context).inflate(R.layout.template_animal_tag, parent, false).text_animal_tag_template
+                tagView.text = it
+                binding.layoutAnimalTags.addView(tagView)
+
+                val spaceView = LayoutInflater.from(context).inflate(R.layout.template_space, parent, false)
+                binding.layoutAnimalTags.addView(spaceView)
+            }
+        }
 
         return convertView ?: binding.root
     }
