77688323041de58fef1137d51c29603bdcfa6775
YuKitAs <moncher.merci@gmail.com>
2019-04-07 18:05:05 +0200

    Fix package name

--- client/app/src/androidTest/java/yukitas/animal/collector/ExampleInstrumentedTest.kt
+++ client/app/src/androidTest/java/yukitas/animal/collector/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("yukitas.animal_collector", appContext.packageName)
+        assertEquals("yukitas.animal.collector", appContext.packageName)
     }
 }
