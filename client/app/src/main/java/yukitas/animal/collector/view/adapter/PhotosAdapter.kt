cdcee7308d711aeed5894e2119d6364985a8c04d
YuKitAs <moncher.merci@gmail.com>
2019-04-12 01:00:01 +0200

    Refactor to use util function

--- client/app/src/main/java/yukitas/animal/collector/view/adapter/PhotosAdapter.kt
+++ client/app/src/main/java/yukitas/animal/collector/view/adapter/PhotosAdapter.kt
@@ -3,14 +3,13 @@ package yukitas.animal.collector.view.adapter
 import android.annotation.SuppressLint
 import android.content.Context
 import android.databinding.DataBindingUtil
-import android.graphics.BitmapFactory
-import android.util.Base64
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import yukitas.animal.collector.R
 import yukitas.animal.collector.model.Photo
+import yukitas.animal.collector.utility.toBitmap
 
 class PhotosAdapter(private val context: Context) : BaseAdapter() {
     private lateinit var binding: yukitas.animal.collector.databinding.ItemPhotoBinding
@@ -29,18 +28,13 @@ class PhotosAdapter(private val context: Context) : BaseAdapter() {
 
     @SuppressLint("ViewHolder")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.item_photo, parent, false)
+        binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.item_photo, parent,
+                false)
 
         val photo = photos[position]
         binding.photo = photo
-
-        setThumbnail(photo.content)
+        binding.imagePhotoThumbnail.setImageBitmap(toBitmap(photo.content))
 
         return convertView ?: binding.root
-    }
-
-    private fun setThumbnail(content: String) {
-        val photoContent = Base64.decode(content.toByteArray(), Base64.NO_WRAP)
-        binding.imagePhotoThumbnail.setImageBitmap(BitmapFactory.decodeByteArray(photoContent, 0, photoContent.size))
     }
 }
