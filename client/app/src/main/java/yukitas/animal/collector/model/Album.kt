7a7d52a81fadd73cbec881b98cac73f2bf5e0ac3
YuKitAs <moncher.merci@gmail.com>
2019-06-20 23:42:20 +0200

    Add category to album and animal models

--- client/app/src/main/java/yukitas/animal/collector/model/Album.kt
+++ client/app/src/main/java/yukitas/animal/collector/model/Album.kt
@@ -2,6 +2,7 @@ package yukitas.animal.collector.model
 
 data class Album(
         val id: String,
+        val category: Category,
         var name: String,
         val photos: List<Photo>,
         var thumbnail: Photo?
--- client/app/src/main/java/yukitas/animal/collector/model/Animal.kt
+++ client/app/src/main/java/yukitas/animal/collector/model/Animal.kt
@@ -1,7 +1,8 @@
 package yukitas.animal.collector.model
 
-data class Animal (
+data class Animal(
         val id: String,
+        val category: Category,
         var name: String,
         var tags: List<String>,
         var thumbnail: Photo?
