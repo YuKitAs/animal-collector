26c423bcd6ba6fc8422ac8ede340982480bcd48d
YuKitAs <moncher.merci@gmail.com>
2019-07-09 00:45:39 +0200

    Minor refactoring

--- client/app/src/main/java/yukitas/animal/collector/model/dto/SavePhotoRequest.kt
+++ client/app/src/main/java/yukitas/animal/collector/model/dto/SavePhotoRequest.kt
@@ -1,4 +1,5 @@
 package yukitas.animal.collector.model.dto
 
-data class SavePhotoRequest(val animalIds: List<String>, val albumIds: List<String>,
+data class SavePhotoRequest(val albumIds: List<String>,
+                            val animalIds: List<String>,
                             val description: String)
