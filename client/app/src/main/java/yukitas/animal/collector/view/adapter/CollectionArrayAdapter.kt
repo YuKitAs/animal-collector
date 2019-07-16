5cb022c1b2a8dfa8f917f9fb6a2c811baf025e25
YuKitAs <moncher.merci@gmail.com>
2019-07-17 00:45:23 +0200

    Add custom array adapter

---
+++ client/app/src/main/java/yukitas/animal/collector/view/adapter/CollectionArrayAdapter.kt
@@ -0,0 +1,33 @@
+package yukitas.animal.collector.view.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import yukitas.animal.collector.model.Collection
+
+class CollectionArrayAdapter(context: Context, var resource: Int, var textViewResourceId: Int,
+                             private var collections: ArrayList<Collection>) : ArrayAdapter<Collection>(
+        context,
+        resource, textViewResourceId, collections) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view: View
+        if (convertView == null) {
+            view = LayoutInflater.from(context).inflate(resource, parent, false)
+            view.findViewById<TextView>(textViewResourceId).text = getItem(position).name
+        } else {
+            view = convertView
+        }
+
+        return view
+    }
+
+    override fun getItem(position: Int): Collection = collections[position]
+
+    override fun getItemId(position: Int): Long = position.toLong()
+
+    override fun getCount(): Int = collections.size
+}
