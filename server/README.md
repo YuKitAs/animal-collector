62bb7c7668e982ef139205a73279467acaf839ab
YuKitAs <moncher.merci@gmail.com>
2019-07-27 21:18:16 +0200

    Modify schema, update README

--- server/README.md
+++ server/README.md
@@ -4,25 +4,24 @@
 
 1. Setup database:
 
-    1.1 Run Postgres image and enter container with default user `postgres`:
-    ```console
-    $ docker run --name postgres-dev -p 5432:5432 -d postgres
-    $ docker exec -it postgres-dev su - postgres
-    ```
+    * Run Postgres image and create database with default user `postgres`:
+        ```console
+        $ docker run --name postgres-dev -p 5432:5432 -e POSTGRES_DB=ani_co_dev -d postgres
+        ```
     
-    1.2 In container, create database `ani_co_dev`:
-    ```
-    $ psql
-    postgres=# CREATE DATABASE ani_co_dev;
-    ```
-    
+    * Enter database as user `postgres`:
+        ```
+        $ docker exec -it postgres-dev su postgres
+        $ psql
+        postgres=# \c ani_co_dev
+        ```
 
-2. Set the property value of `spring.jpa.hibernate.ddl-auto` to `create` in order to initialize the tables.
-
-3. Run application:
+2. Run application:
 
     ```console
     $ ./gradlew bootRun
     ```
+    
+    For initializing the schema as defined in `src/main/resources/schema.sql`, set `spring.jpa.hibernate.ddl-auto` to `none`.
 
 For running tests, create database `ani_co_test` and run application with profile `test`.
--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -1,47 +1,35 @@
 -- categories
-CREATE TABLE  IF NOT EXISTS  public.categories (
+CREATE TABLE  IF NOT EXISTS public.categories (
     id uuid NOT NULL,
-    name character varying(64)
+    name character varying(64),
+    CONSTRAINT categories_pkey PRIMARY KEY (id),
+    CONSTRAINT uk_categories_name UNIQUE (name)
 );
 
 ALTER TABLE public.categories OWNER TO postgres;
-
-ALTER TABLE ONLY public.categories
-    ADD CONSTRAINT categories_pkey PRIMARY KEY (id);
-
-ALTER TABLE ONLY public.categories
-    ADD CONSTRAINT uk_categories_name UNIQUE (name);
 
 -- albums
 CREATE TABLE IF NOT EXISTS public.albums (
     id uuid NOT NULL,
     name character varying(64),
-    category_id uuid NOT NULL
+    category_id uuid NOT NULL,
+    CONSTRAINT albums_pkey PRIMARY KEY (id),
+    CONSTRAINT fk_albums_category_id_categories_id FOREIGN KEY (category_id) REFERENCES public.categories(id) ON DELETE CASCADE
 );
 
 ALTER TABLE public.albums OWNER TO postgres;
-
-ALTER TABLE ONLY public.albums
-    ADD CONSTRAINT albums_pkey PRIMARY KEY (id);
-
-ALTER TABLE ONLY public.albums
-    ADD CONSTRAINT fk_albums_category_id_categories_id FOREIGN KEY (category_id) REFERENCES public.categories(id) ON DELETE CASCADE;
 
 -- animals
 CREATE TABLE IF NOT EXISTS public.animals (
     id uuid NOT NULL,
     name character varying(32),
     tags bytea,
-    category_id uuid NOT NULL
+    category_id uuid NOT NULL,
+    CONSTRAINT animals_pkey PRIMARY KEY (id),
+    CONSTRAINT fk_animals_category_id_categories_id FOREIGN KEY (category_id) REFERENCES public.categories(id) ON DELETE CASCADE
 );
 
 ALTER TABLE public.animals OWNER TO postgres;
-
-ALTER TABLE ONLY public.animals
-    ADD CONSTRAINT animals_pkey PRIMARY KEY (id);
-
-ALTER TABLE ONLY public.animals
-    ADD CONSTRAINT fk_animals_category_id_categories_id FOREIGN KEY (category_id) REFERENCES public.categories(id) ON DELETE CASCADE;
 
 -- photos
 CREATE TABLE IF NOT EXISTS public.photos (
@@ -51,47 +39,33 @@ CREATE TABLE IF NOT EXISTS public.photos (
     description character varying(255),
     address character varying(255),
     latitude double precision,
-    longitude double precision
+    longitude double precision,
+    CONSTRAINT photos_pkey PRIMARY KEY (id)
 );
 
 ALTER TABLE public.photos OWNER TO postgres;
 
-ALTER TABLE ONLY public.photos
-    ADD CONSTRAINT photos_pkey PRIMARY KEY (id);
-
 -- photo_animal
 CREATE TABLE IF NOT EXISTS public.photo_animal (
     photo_id uuid NOT NULL,
-    animal_id uuid NOT NULL
+    animal_id uuid NOT NULL,
+    CONSTRAINT photo_animal_pkey PRIMARY KEY (photo_id, animal_id),
+    CONSTRAINT fk_photo_animal_photo_id_photos_id FOREIGN KEY (photo_id) REFERENCES public.photos(id),
+    CONSTRAINT fk_photo_animal_animal_id_animals_id FOREIGN KEY (animal_id) REFERENCES public.animals(id)
 );
 
 ALTER TABLE public.photo_animal OWNER TO postgres;
 
-ALTER TABLE ONLY public.photo_animal
-    ADD CONSTRAINT photo_animal_pkey PRIMARY KEY (photo_id, animal_id);
-
-ALTER TABLE ONLY public.photo_animal
-    ADD CONSTRAINT fk_photo_animal_photo_id_photos_id FOREIGN KEY (photo_id) REFERENCES public.photos(id);
-
-ALTER TABLE ONLY public.photo_animal
-    ADD CONSTRAINT fk_photo_animal_animal_id_animals_id FOREIGN KEY (animal_id) REFERENCES public.animals(id);
-
 -- photo_album
 CREATE TABLE IF NOT EXISTS public.photo_album (
     photo_id uuid NOT NULL,
-    album_id uuid NOT NULL
+    album_id uuid NOT NULL,
+    CONSTRAINT photo_album_pkey PRIMARY KEY (photo_id, album_id),
+    CONSTRAINT fk_photo_album_photo_id_photos_id FOREIGN KEY (photo_id) REFERENCES public.photos(id),
+    CONSTRAINT fk_photo_album_album_id_albums_id FOREIGN KEY (album_id) REFERENCES public.albums(id)
 );
 
 ALTER TABLE public.photo_album OWNER TO postgres;
-
-ALTER TABLE ONLY public.photo_album
-    ADD CONSTRAINT photo_album_pkey PRIMARY KEY (photo_id, album_id);
-
-ALTER TABLE ONLY public.photo_album
-    ADD CONSTRAINT fk_photo_album_photo_id_photos_id FOREIGN KEY (photo_id) REFERENCES public.photos(id);
-
-ALTER TABLE ONLY public.photo_album
-    ADD CONSTRAINT fk_photo_album_album_id_albums_id FOREIGN KEY (album_id) REFERENCES public.albums(id);
 
 -- trigger
 CREATE OR REPLACE FUNCTION delete_obsolete_photos() RETURNS trigger AS
@@ -103,6 +77,8 @@ CREATE OR REPLACE FUNCTION delete_obsolete_photos() RETURNS trigger AS
       RETURN NULL;
     END
 ' LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS trigger_delete_obsolete_photos ON photos;
 
 CREATE TRIGGER trigger_delete_obsolete_photos
     AFTER INSERT ON photos
