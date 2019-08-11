bf7b221d0327ba95a914a28ec2d8c22617ea7190
YuKitAs <moncher.merci@gmail.com>
2019-08-12 00:24:57 +0200

    Extract activity results to constants

--- client/app/src/main/java/yukitas/animal/collector/common/Constants.kt
+++ client/app/src/main/java/yukitas/animal/collector/common/Constants.kt
@@ -11,6 +11,14 @@ class Constants {
         const val ARG_PHOTO_ID = "photo_id"
         const val ARG_PHOTO_DESC = "photo_desc"
         const val ARG_IS_CREATING = "is_creating"
-        const val ARG_DETECTED_CATEGORY = "detected_category"
+        const val ARG_RECOGNIZED_CATEGORY = "recognized_category"
+
+        const val CATEGORY_UNKNOWN = "UNKNOWN"
+
+        const val RESULT_LOAD_IMAGE = 1
+        const val RESULT_EDIT_ALBUM = 2
+        const val RESULT_EDIT_ANIMAL = 2
+        const val RESULT_CREATE_ANIMAL = 3
+        const val RESULT_CREATE_ALBUM = 3
     }
 }
