7e52fefb117d2cd3f4e135983086caa04a3c996b
YuKitAs <moncher.merci@gmail.com>
2019-08-12 01:18:51 +0200

    Rename constants

--- client/app/src/main/java/yukitas/animal/collector/common/Constants.kt
+++ client/app/src/main/java/yukitas/animal/collector/common/Constants.kt
@@ -3,6 +3,8 @@ package yukitas.animal.collector.common
 class Constants {
     companion object {
         const val ARG_CATEGORY_ID = "category_id"
+        const val ARG_CATEGORY_NAME = "category_name"
+        const val CATEGORY_UNKNOWN = "unknown"
         const val ARG_ALBUM_ID = "album_id"
         const val ARG_ALBUM_NAME = "album_name"
         const val ARG_ANIMAL_ID = "animal_id"
@@ -10,10 +12,8 @@ class Constants {
         const val ARG_ANIMAL_TAGS = "animal_tags"
         const val ARG_PHOTO_ID = "photo_id"
         const val ARG_PHOTO_DESC = "photo_desc"
-        const val ARG_IS_CREATING = "is_creating"
-        const val ARG_RECOGNIZED_CATEGORY = "recognized_category"
 
-        const val CATEGORY_UNKNOWN = "UNKNOWN"
+        const val ARG_IS_CREATING = "is_creating"
 
         const val RESULT_LOAD_IMAGE = 1
         const val RESULT_EDIT_ALBUM = 2
