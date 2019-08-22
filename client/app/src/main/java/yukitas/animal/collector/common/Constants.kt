be81759d80b5dfd61352cee8f96876fef692802a
YuKitAs <moncher.merci@gmail.com>
2019-08-23 00:58:05 +0200

    Add new flag keys

--- client/app/src/main/java/yukitas/animal/collector/common/Constants.kt
+++ client/app/src/main/java/yukitas/animal/collector/common/Constants.kt
@@ -2,11 +2,12 @@ package yukitas.animal.collector.common
 
 class Constants {
     companion object {
+        const val CATEGORY_UNKNOWN = "unknown"
+
         const val ARG_CATEGORY_CAT = "cat"
         const val ARG_CATEGORY_DOG = "dog"
         const val ARG_CATEGORY_ID = "category_id"
         const val ARG_CATEGORY_NAME = "category_name"
-        const val CATEGORY_UNKNOWN = "unknown"
         const val ARG_ALBUM_ID = "album_id"
         const val ARG_ALBUM_NAME = "album_name"
         const val ARG_ANIMAL_ID = "animal_id"
@@ -15,7 +16,9 @@ class Constants {
         const val ARG_PHOTO_ID = "photo_id"
         const val ARG_PHOTO_DESC = "photo_desc"
 
-        const val ARG_IS_CREATING = "is_creating"
+        const val FLAG_IS_CREATING = "is_creating"
+        const val FLAG_RECOGNITION_ENABLED = "recognition_enabled"
+        const val FLAG_CATEGORY_CONFIRMED = "category_confirmed"
 
         const val RESULT_LOAD_IMAGE = 1
         const val RESULT_EDIT_ALBUM = 2
